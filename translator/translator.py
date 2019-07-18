3ffdb63a94a226d754dabd999a72c0055b424f4d
yantang <190626777@qq.com>
2019-07-18 19:27:15 +0800

    Add timer, Thread for start, and fix Frame info.

--- translator/translator.py
+++ translator/translator.py
@@ -6,7 +6,23 @@ import time
 import random
 import hashlib
 import requests
-from tkinter import *
+import sys
+import tkinter as tk
+from threading import Thread
+from functools import wraps
+
+
+def timer(func):
+    @wraps(func)
+    def function_timer(*args, **kwargs):
+        start_t = time.time()
+        result = func(*args, **kwargs)
+        end_t = time.time()
+        word = kwargs.get("word", '')
+        print("Function: {name} finished, spent time: {time:.5f}s. {word}".format(
+            name=func.__name__, time=end_t - start_t, word=word))
+        return result
+    return function_timer
 
 
 class Youdao:
@@ -85,8 +101,14 @@ class Baidu:
         }
         self.url = 'https://fanyi.baidu.com/v2transapi'
         self.url_ch2en = "https://fanyi.baidu.com/#zh/en/"
+        '''
+        start = Thread(target=self.translate, args=("start", ))
+        start.start()
+        '''
+        # self.translate("start")
 
     def get_page(self, ch_word):
+        """no use."""
         res = requests.post(self.url_ch2en + ch_word, headers=self.headers)
         res.encoding = res.apparent_encoding
         # then res.text is charset type.
@@ -95,6 +117,7 @@ class Baidu:
         content = res.content.decode('utf-8')
         return res.text
 
+    @timer
     def translate(self, word):
         self.data['query'] = word
         self.data['token'], gtk = self.__get_token_gtk()
@@ -104,22 +127,23 @@ class Baidu:
         self.data['sign'] = self.__get_sign(gtk, word)
         res = self.session.post(self.url, data=self.data)
         all_data = res.json()
-        print(all_data)
+        # print(all_data)
         trans_result = all_data.get("trans_result").get("data")[0].get("result")[0][1]
-        double_result = all_data.get("liju_result").get("double")
-        tag_result = all_data.get("liju_result").get("tag")
-        print(tag_result)
+        double_result = all_data.get("liju_result").get("double", None)
+        tag_result = all_data.get("liju_result").get("tag", None)
         double_example_list = []
         # print(self.data)
-        for tup in json.loads(double_result):
-            juzi = self.__get_sentence(tup[0])
-            sentence = self.__get_sentence(tup[1])
-            double_example_list.append((juzi, sentence))
+        if double_result is not None and len(double_result):
+            for tup in json.loads(double_result):
+                juzi = self.__get_sentence(tup[0])
+                sentence = self.__get_sentence(tup[1])
+                double_example_list.append((juzi, sentence))
         return {"trans_result": trans_result,
                 "double_example": double_example_list,
                 "tag_result": tag_result}
         # return [res.json()['trans_result']['data'][0]['result'][0][1]]
 
+    @timer
     def __get_token_gtk(self):
         url = 'https://fanyi.baidu.com/'
         res = requests.get(url, headers=self.headers)
@@ -128,6 +152,7 @@ class Baidu:
         return token, gtk
 
     @staticmethod
+    @timer
     def __get_sign(gtk, word):
         evaljs = js2py.EvalJs()
         js_code = bd_js_code
@@ -158,91 +183,101 @@ class Baidu:
         return sentence
 
 
-class Application(Frame):
+class Application(tk.Frame):
     def __init__(self, master=None):
-        Frame.__init__(self, master)
-        self.pack(fill=BOTH, expand=YES)
+        tk.Frame.__init__(self, master)
+        self.pack(fill="both", expand="yes")
         # self.text = StringVar()
-        self.__create_widgets()
         self.baidu_translator = Baidu()
+        '''It will take more times in the first search. So use a thread here.'''
+        first_search = Thread(target=self.baidu_translator.translate, args=("start",))
+        first_search.start()
+        self.__create_widgets()
 
     def __create_widgets(self):
         """"""
         '''frame1: title'''
-        self.frame1 = Frame(self)
-        self.title_label = Label(self.frame1, text="Translate", font=("微软雅黑", 15), width='10')
+        self.frame1 = tk.Frame(self)
+        self.title_label = tk.Label(self.frame1, text="Translate", font=("微软雅黑", 15), width='10')
         self.title_label.pack()
-        self.frame1.pack(side=TOP)
+        self.frame1.pack(side="top")
         '''frame2: search text and button'''
-        self.frame2 = Frame(self)
-        self.frame2_left = Frame(self.frame2)
-        self.frame2_right = Frame(self.frame2)
-        self.search_text = Entry(self.frame2_left, font=("", 13), width='20')
-        self.button = Button(self.frame2_right, text='search', font=("", 15), width='7', command=self.__translate)
+        self.frame2 = tk.Frame(self)
+        self.frame2_left = tk.Frame(self.frame2)
+        self.frame2_right = tk.Frame(self.frame2)
+        self.search_text = tk.Entry(self.frame2_left, font=("", 13), width='20')
+        self.button = tk.Button(self.frame2_right, text='search', font=("", 15), width='7',
+                                cursor="hand2", command=self.__translate)
         '''Press button or ENTER key'''
         self.search_text.bind('<Return>', self.__enter)
         self.search_text.pack()
         self.button.pack()
-        self.frame2_left.pack(side=LEFT)
-        self.frame2_right.pack(side=RIGHT)
-        self.frame2.pack(side=TOP, ipadx=3, pady=2)
+        self.frame2_left.pack(side="left")
+        self.frame2_right.pack(side="right")
+        self.frame2.pack(side="top", ipadx=3, pady=2)
         '''frame3: answer word'''
-        self.frame3 = Frame(self)
-        self.trans_answer = Entry(self.frame3, font=("", 13), width='30')
-        self.smart_answer = Entry(self.frame3, font=("", 13), width='60')
-        self.trans_answer.pack(side=TOP, pady=3)
-        self.smart_answer.pack(side=TOP, padx=10, fill=X)
-        self.frame3.pack(side=TOP, fill=BOTH, ipady=3)
+        self.frame3 = tk.Frame(self)
+        self.trans_answer = tk.Entry(self.frame3, font=("Times", 13), width='30')
+        self.smart_answer = tk.Entry(self.frame3, font=("Times", 13), width='60')
+        self.trans_answer.pack(side="top", pady=3)
+        self.smart_answer.pack(side="top", padx=10, fill='x')
+        self.frame3.pack(side="top", fill="both", ipady=3)
         '''frame4 answer text area'''
-        self.frame4 = Frame(self)
-        self.y_scrollbar = Scrollbar(self.frame4)
-        self.x_scrollbar = Scrollbar(self.frame4, orient=HORIZONTAL)
-        self.search_answer = Text(self.frame4)
+        self.frame4 = tk.Frame(self)
+        self.y_scrollbar = tk.Scrollbar(self.frame4)
+        self.x_scrollbar = tk.Scrollbar(self.frame4, orient="horizontal")
+        self.search_answer = tk.Text(self.frame4)
         # Combine scrollbar with text.
         self.y_scrollbar.config(command=self.search_answer.yview)
         self.x_scrollbar.config(command=self.search_answer.xview)
         # If wrap = 'none' in config. text would show in one line.
         self.search_answer.config(xscrollcommand=self.x_scrollbar.set, yscrollcommand=self.y_scrollbar.set,
-                                  font=("", 12), width='60', wrap=NONE)
+                                  font=("Times", 12), width='60', wrap="none")
         # Pack order is important.
-        self.x_scrollbar.pack(side=BOTTOM, fill=BOTH)
-        self.y_scrollbar.pack(side=RIGHT, fill=BOTH)
-        self.search_answer.pack(fill=BOTH, expand=YES)
-        self.frame4.pack(expand=YES, fill=BOTH, anchor=CENTER)
+        self.x_scrollbar.pack(side="bottom", fill="both")
+        self.y_scrollbar.pack(side="right", fill="both")
+        self.search_answer.pack(fill="both", expand="yes")
+        self.frame4.pack(expand="yes", fill="both", anchor="center")
         # Init state.
-        self.trans_answer.config(state=DISABLED)
-        self.smart_answer.config(state=DISABLED)
-        self.search_answer.config(state=DISABLED)
+        self.trans_answer.config(state="disabled")
+        self.smart_answer.config(state="disabled")
+        self.search_answer.config(state="disabled")
         self.search_text.focus()
 
     def __enter(self, event):
         print(event)
         self.__translate()
 
+    @timer
     def __translate(self):
         # self.update()
         info = self.search_text.get()
-        res = self.baidu_translator.translate(info)
+        res = self.baidu_translator.translate(word=info)
         trans_result = res.get("trans_result", None)
         double_example = res.get("double_example", None)
         tag_result = res.get("tag_result", None)
         # text = ""
         '''clear'''
-        self.trans_answer.config(state=NORMAL)
-        self.smart_answer.config(state=NORMAL)
-        self.search_answer.config(state=NORMAL)
-        self.trans_answer.delete(0, END)
-        self.smart_answer.delete(0, END)
-        self.search_answer.delete(1.0, END)
 
-        self.trans_answer.insert(END, trans_result)
-        for word in tag_result:
-            self.smart_answer.insert(END, "{}  ".format(word))
-        for idx, example in enumerate(double_example, 1):
-            print("{}.\t{}\n\t{}".format(idx, example[0], example[1]))
-            # text += "{}.\t{}\n\t{}\n".format(idx, example[0], example[1])
-            self.search_answer.insert(END, "{}. {}\n  {}\n".format(idx, example[0], example[1]))
-        self.search_answer.config(state=DISABLED)
+        self.trans_answer.config(state="normal")
+        self.smart_answer.config(state="normal")
+        self.search_answer.config(state="normal")
+        self.trans_answer.delete(0, "end")
+        self.smart_answer.delete(0, "end")
+        self.search_answer.delete(1.0, "end")
+
+        self.trans_answer.insert("end", trans_result)
+        try:
+            for word in tag_result:
+                self.smart_answer.insert("end", "{}  ".format(word))
+            for idx, example in enumerate(double_example, 1):
+                # print("{}.\t{}\n\t{}".format(idx, example[0], example[1]))
+                # text += "{}.\t{}\n\t{}\n".format(idx, example[0], example[1])
+                self.search_answer.insert("end", "{}. {}\n  {}\n".format(idx, example[0], example[1]))
+        except Exception as e:
+            # sys._getframe().f_code.co_name
+            print("{} in {}".format(e, self.__class__.__name__))
+        self.search_answer.config(state="disabled")
         # self.trans_answer.config(state=DISABLED)
         # self.smart_answer.config(state=DISABLED)
 
@@ -256,6 +291,7 @@ if __name__ == "__main__":
     # app.master.geometry('800x500')
     app.master.minsize(width=400, height=300)
     app.mainloop()
+    print()
     '''
     bd = Baidu()
     while 1:
